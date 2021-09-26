f8f772490a31accd56eedf1ccc6e1dc3d7e1448c
MaddieSavic <30588636+MaddieSavic@users.noreply.github.com>
2021-09-26 13:45:20 +1000

    got map loading on index.html

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "choroplethmap.vg.json";
+var vg_1 = "js/choroplethmap.vg.json";
 vegaEmbed("#importMap", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
